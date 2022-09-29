0f9155ee6dbfc68f7656ae28932d7ed74ef6fa61
newsoft111 <ydj0617@gmail.com>
2022-09-29 17:08:05 +0900

    a

--- product/views.py
+++ product/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
+from django.db.models import Q
+from django.core.paginator import Paginator
 from .models import *
 # Create your views here.
 def product_list(request):
@@ -7,12 +9,19 @@ def product_list(request):
 		'title': "상품 리스트 - 유토빌",
 	}
 
-	return render(request, 'shop/product_list.html' ,{
+	q = Q()
+	#q &= Q(is_deleted = True)
+
+
+	product_list =  Product.objects.filter(q).order_by( "-id")
+	page        = int(request.GET.get('p', 1))
+	pagenator   = Paginator(product_list, 12)
+	product_list = pagenator.get_page(page)
+
+	return render(request, 'product/product_list.html' ,{
 		"seo":seo,
+		"product_list":product_list,
 	})
 
 
-class ProductLV(ListView):
-	model = Product
-	print(model)
 
