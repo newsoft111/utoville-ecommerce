b11c2092ead959af15e456990c059bf04bb54915
newsoft111 <ydj0617@gmail.com>
2022-09-30 10:15:34 +0900

    Update views.py

--- product/views.py
+++ product/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import TemplateView, ListView
 from django.db.models import Q
 from django.core.paginator import Paginator
 from .models import *
