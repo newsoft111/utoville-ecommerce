59064ee4afaf16d8c2cb3e453d996d9ae149f86b
Ohreum <dhfma3394@naver.com>
2022-09-30 11:53:23 +0900

    a

--- product/views.py
+++ product/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
+from django.core.paginator import Paginator
 from .models import *
 # Create your views here.
 def product_list(request):
@@ -7,24 +8,30 @@ def product_list(request):
 		'title': "상품 리스트 - 유토빌",
 	}
 
-	return render(request, 'shop/product_list.html' ,{
+	q = Q()
+	#q &= Q(is_deleted = True)
+
+
+	product_list =  Product.objects.filter(q).order_by( "-id")
+	page        = int(request.GET.get('p', 1))
+	pagenator   = Paginator(product_list, 12)
+	product_list = pagenator.get_page(page)
+
+	return render(request, 'product/product_list.html' ,{
 		"seo":seo,
+		"product_list":product_list,
 	})
 
 
-class ProductLV(ListView):
-	model = Product
+def product_detail(request, product_id):
+	seo = {
+		'title': "상품 디테일 - 유토빌",
+	}
 
-<<<<<<< Updated upstream
-=======
 	product_detail = get_object_or_404(Product, pk=product_id)
+
 
 	return render(request, 'product/product_detail.html' ,{
 		"seo":seo,
 		"product_detail": product_detail,
-<<<<<<< Updated upstream
 	})
->>>>>>> Stashed changes
-=======
-	})
->>>>>>> Stashed changes
