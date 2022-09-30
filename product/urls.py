eb1c162625e792bf4a7f2813c5e42d847fdb9e2c
newsoft111 <ydj0617@gmail.com>
2022-09-30 10:08:55 +0900

    Update urls.py

--- product/urls.py
+++ product/urls.py
@@ -5,5 +5,5 @@ from .views import *
 
 urlpatterns = [
 	path('', product_list, name="list"),
-	path('<int:product_id>/', product_detail, name="detail"),
+	path('1/', product_detail, name="detail"),
 ]
