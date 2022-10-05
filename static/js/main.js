970ad2c67c015b51d0eaf2b98ef6d9062dcf1d55
Ohreum <dhfma3394@naver.com>
2022-10-05 17:52:47 +0900

    10-05
    주문조회 진행중

--- static/js/main.js
+++ static/js/main.js
@@ -48,3 +48,13 @@ $(document).ready(function() {
 	}
 	$('#closeModalBtn').attr('data-before-modal','');
  }
+
+ $(document).ready(function(){
+
+	$('.input-daterange').datepicker({
+		format: 'dd-mm-yyyy',
+		todayHighlight: true,
+		startDate: '0d'
+	});
+	
+});
