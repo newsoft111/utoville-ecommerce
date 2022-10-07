f9f25e97af2279a8b275d722b63658610d9c9c2e
newsoft111 <ydj0617@gmail.com>
2022-10-07 18:17:54 +0900

    z

--- product/views.py
+++ product/views.py
@@ -9,26 +9,29 @@ def product_list(request):
 		'title': "상품 리스트 - 유토빌",
 	}
 
-	category_first_list =  CategoryFirst.objects.all().order_by( "-id")
+	category_first_list =  CategoryFirst.objects.all().order_by( "id")
 
 	q = Q()
-	if request.GET.get("category_1"):
-		q &= Q(parent = int(request.GET.get("category_1")))
-		category_second_list =  CategorySecond.objects.filter(q).order_by( "-id")
+	if request.GET.get("category1"):
+		q &= Q(parent = int(request.GET.get("category1")))
+		category_second_list =  CategorySecond.objects.filter(q).order_by( "id")
 	else:
 		category_second_list = None
 
 
 
 	q = Q()
-	if request.GET.get("category_1"):
-		q &= Q(category = int(request.GET.get("category_1")))
-	if request.GET.get("category_2"):
-		q &= Q(category = int(request.GET.get("category_2")))
-	if request.GET.get("category_3"):
-		q &= Q(category = int(request.GET.get("category_3")))
+	if request.GET.get("category1"):
+		q &= Q(category_first = int(request.GET.get("category1")))
+	if request.GET.get("category2"):
+		q &= Q(category_second = int(request.GET.get("category2")))
+	if request.GET.get("category3"):
+		q &= Q(category_third = int(request.GET.get("category3")))
+	if request.GET.get("area"):
+		q &= Q(productarea__area__icontains = request.GET.get("area"))
 
 	product_list =  Product.objects.filter(q).order_by( "-id")
+
 	page        = int(request.GET.get('p', 1))
 	pagenator   = Paginator(product_list, 12)
 	product_list = pagenator.get_page(page)
