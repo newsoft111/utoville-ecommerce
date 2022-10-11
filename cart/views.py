75c360ae8e488885f2a7895044dcb116dd490511
newsoft111 <ydj0617@gmail.com>
2022-10-11 17:51:42 +0900

    나는 문어

--- cart/views.py
+++ cart/views.py
@@ -25,6 +25,23 @@ def get_user_cart(request):
 	return cart
 
 
+def get_cart_info(request):
+	total_price=0
+	total_quantity=0
+
+	try:
+		cart_items = CartItem.objects.filter(cart=get_user_cart(request), active=True)
+		for cart_item in cart_items:
+			total_price += (cart_item.product.price * cart_item.quantity)
+			total_quantity += cart_item.quantity
+	except ObjectDoesNotExist:
+		pass
+		
+	return {
+		"total_price": total_price,
+		"total_quantity": total_quantity
+	}
+
 
 def add_cart(request):
 	cart = get_user_cart(request)
@@ -45,16 +62,55 @@ def add_cart(request):
 			quantity=1
 		)
 
-
 	result = '200'
 	result_text = '장바구니 넣었음.'
 
 	result = {'result': result, 'result_text': result_text}
 	return JsonResponse(result)
+
+
+def remove_cart(request):
+	cart = get_user_cart(request)
+
+	cart_item_id=request.POST.get('cart_item_id')
+
+	cart_item = CartItem.objects.get(pk=cart_item_id, cart=cart)
+	cart_item.delete()
+	
+	result = '200'
+	result_text = '장바구니 삭제함.'
+
+	result = {'result': result, 'result_text': result_text}
+	return JsonResponse(result)
+
+
+def update_cart(request):
+	cart = get_user_cart(request)
+
+	cart_item_id=request.POST.get('cart_item_id')
+	quantity = int(request.POST.get('qty')) or 1
+
+	cart_item = CartItem.objects.get(pk=cart_item_id, cart=cart)
+	cart_item.quantity = quantity
+	cart_item.save()
+
+	cart_info = get_cart_info(request)
+	total_price = cart_info['total_price']
+	total_quantity = cart_info['total_quantity']
+
+	result = '200'
+	result_text = '장바구니 업데이트 되었음.'
+	custom_data = {
+		"total_price": format(total_price, ','),
+		"total_quantity": total_quantity
+	}
+
+	result = {'result': result, 'result_text': result_text, "custom_data": custom_data}
+	return JsonResponse(result)
 	
 
 
-def cart_detail(request, total=0, counter=0, cart_items=None):
+def cart_detail(request, total_price=0, counter=0, cart_items=None):
 	seo = {
 		'title': "장바구니 - 유토빌",
 	}
@@ -62,18 +118,21 @@ def cart_detail(request, total=0, counter=0, cart_items=None):
 	try:
 		cart_items = CartItem.objects.filter(cart=get_user_cart(request), active=True)
 		for cart_item in cart_items:
-			total += (cart_item.product.price * cart_item.quantity)
+			total_price += (cart_item.product.price * cart_item.quantity)
 			counter += cart_item.quantity
 	except ObjectDoesNotExist:
 		pass
-	
+
+	cart_info = get_cart_info(request)
+	total_price = cart_info['total_price']
+	total_quantity = cart_info['total_quantity']
 
 	return render(request, 'cart/cart_detail.html' ,
 		dict(
 			seo = seo,
 			cart_items = cart_items,
-			total = total,
-			counter = counter
+			total_price = total_price,
+			total_quantity = total_quantity
 		)
 	)
 
--- product/views.py
+++ product/views.py
@@ -28,7 +28,7 @@ def product_list(request):
 	if request.GET.get("category3"):
 		q &= Q(category_third = int(request.GET.get("category3")))
 	if request.GET.get("area"):
-		q &= Q(productarea__area__icontains = request.GET.get("area"))
+		q &= Q(productarea__area = request.GET.get("area"))
 
 	product_list =  Product.objects.filter(q).order_by( "-id")
 
