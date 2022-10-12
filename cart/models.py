8f84d9ac808e80a20482e85e403316b9e79c16de
newsoft111 <ydj0617@gmail.com>
2022-10-12 13:48:53 +0900

    ㅋ

--- cart/models.py
+++ cart/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from product.models import Product
+from product.models import Product, ProductVariantValue
 from datetime import datetime
 from django.conf import settings
 
@@ -25,13 +25,19 @@ class CartItem(models.Model):
 	product = models.ForeignKey(Product, on_delete=models.CASCADE)
 	cart = models.ForeignKey(Cart, on_delete=models.CASCADE, null=True, blank=True)
 	quantity = models.PositiveIntegerField()
+	variant_value = models.ForeignKey(
+		ProductVariantValue, 
+		on_delete=models.CASCADE,
+		null=True, 
+		blank=True
+	)
 	active = models.BooleanField(default=True)
 
 	class Meta:
 		db_table = 'ecommerce_cart_item'
 
 	def sub_total(self):
-		return self.product.price * self.quantity
+		return (self.product.price + self.variant_value.price) * self.quantity
 
 	def __str__(self):
 		return self.product
