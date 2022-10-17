762e1bdd9715af14fe833c339459dff8eee04aa8
newsoft111 <ydj0617@gmail.com>
2022-10-17 17:07:26 +0900

    z

---
+++ static/js/custom.js
@@ -0,0 +1,63 @@
+$(document).ready(function() {
+	// a href='#' 클릭 무시 스크립트
+	$('a[href="#"]').click(function(ignore) {
+	   ignore.preventDefault();
+	});
+ });
+ 
+ function isEmpty(str){
+		  
+	if(typeof str == "undefined" || str == null || str == "")
+		  return true;
+	else
+		  return false ;
+ }
+ 
+ function openModal(subject, content, before_modal, option) {
+	$("#modal_subject").text(subject);
+	$("#modal_content").html(content);
+	if (!isEmpty(before_modal)) {
+	   $('#closeModalBtn').attr('data-before-modal',before_modal);
+	}
+	if (option == 'reload') {
+	   document.querySelectorAll('#closeModalBtn').forEach(function(close_btn) {
+		  close_btn.setAttribute( "onClick", "window.location.reload();" );
+	   });
+	} else if (option == 'back') {
+	   document.querySelectorAll('#closeModalBtn').forEach(function(close_btn) {
+		  close_btn.setAttribute( "onClick", "history.back();" );
+	   });
+	} else if (isEmpty(option)) {
+	   console.log('ok');
+	} else {
+	   document.querySelectorAll('#closeModalBtn').forEach(function(close_btn) {
+		  close_btn.setAttribute( "onClick", `${option}` );
+	   });
+	}
+	
+	var modal = new bootstrap.Modal(document.getElementById('globalModal'))
+	modal.show()
+ }
+ 
+function closeModal() {
+	var before_modal = $('#closeModalBtn').attr('data-before-modal');
+	$("#modal_subject").text('');
+	$("#modal_content").html('');
+	if (before_modal) {
+		$(before_modal).modal('show');
+	}
+	
+	$('#closeModalBtn').attr('data-before-modal','');
+	var modal = new bootstrap.Modal(document.getElementById('globalModal'));
+	modal.hide();
+}
+
+ $(document).ready(function(){
+
+	$('.input-daterange').datepicker({
+		format: 'dd-mm-yyyy',
+		todayHighlight: true,
+		startDate: '0d'
+	});
+	
+});
