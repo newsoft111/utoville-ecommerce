0a94b541eed272a59ee27fd276353bd9d3cfc057
newsoft111 <ydj0617@gmail.com>
2022-11-11 17:10:07 +0900

    ㅇ

--- static/js/custom.js
+++ static/js/custom.js
@@ -67,3 +67,10 @@ function getCookie(name) {
     }
     return cookieValue;
 }
+
+
+
+function numberWithCommas(num) { 
+	var parts = num.toString().split("."); 
+	return parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",") + (parts[1] ? "." + parts[1] : ""); 
+}
