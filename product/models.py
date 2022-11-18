af979dd39f1f4b81fa6a9f810f8f78e62b5fcadb
newsoft111 <ydj0617@gmail.com>
2022-11-18 10:11:43 +0900

    p

--- product/models.py
+++ product/models.py
@@ -72,12 +72,17 @@ class ProductArea(models.Model):
 		return self.area
 
 
+
 class ProductVariant(models.Model):
 	product = models.ForeignKey(
 			Product,
 			on_delete=models.CASCADE
 	)
 	variant = models.CharField(max_length=255)
+	is_deleted = models.BooleanField(default=False)
+	deleted_at = models.DateTimeField(null=True)
+
+	objects = ModelDeleteManager()
 
 	class Meta:
 		db_table = 'ecommerce_product_variant'
@@ -93,9 +98,14 @@ class ProductVariantValue(models.Model):
 	)
 	value = models.CharField(max_length=255)
 	price = models.DecimalField(max_digits=14, decimal_places=2)
+	is_deleted = models.BooleanField(default=False)
+	deleted_at = models.DateTimeField(null=True)
 
+	objects = ModelDeleteManager()
+	
 	class Meta:
 		db_table = 'ecommerce_product_variant_value'
+
 
 
 
