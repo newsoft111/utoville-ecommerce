0a9439b51b22a0200cac4639177f2a37ad8b014e
newsoft111 <ydj0617@gmail.com>
2022-11-25 16:08:37 +0900

    마이페이지 리뷰 모델 생성및 기능완료

--- order/urls.py
+++ order/urls.py
@@ -6,5 +6,4 @@ app_name = 'order'
 urlpatterns = [
 	path('', views.order_view, name='view'),
 	path('create/', views.order_create, name='create'),
-	path('chart/done/', views.get_order_done, name='chart_done'),
 ]
