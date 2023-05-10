b18a6088b55e9d46239d4b01a0b6f450fd150f3d
newsoft111 <87762263+newsoft111@users.noreply.github.com>
2023-05-10 13:11:42 +0900

    Update dragon.py

--- charge/dragon.py
+++ charge/dragon.py
@@ -9,65 +9,34 @@ class DragonPay(object):
 	production_url = "https://gw.dragonpay.ph"
 	test_url = "https://test.dragonpay.ph"
 
-	def __init__(self, transaction_id, amount, description, email, param1):
+	def __init__(self):
 		self.merchant_id = 'UTOVILLEPH'
 		#self.secretkey = 'RdcRk3hUxvis8vQ'
 		self.secretkey = 'RdcRk3hUxvis8vQ'
 		production = True
 		self.url = self.production_url if production else self.test_url
 
-		self.transaction_id = transaction_id
-		self.amount = amount
-		self.description = description
-		self.email = email
-		self.param1 = json.dumps(param1)
 
-	def get_token(self):
+
+	def pay(self, transaction_id, amount, description, email, param1):
+		param1 = json.dumps(param1)
+
 		url = f"{self.url}/DragonPayWebService/MerchantService.asmx?wsdl"
 		client = Client(url)
 
 		data = {
 			"merchantId": self.merchant_id,
 			"password": self.secretkey,
-			"merchantTxnId": self.transaction_id,
-			"amount": self.format_amount(self.amount),
+			"merchantTxnId": transaction_id,
+			"amount": self.format_amount(amount),
 			"ccy": "PHP",
-			"description": self.description,
-			"email": self.email,
-			"param1": self.param1,
+			"description": description,
+			"email": email,
+			"param1": param1,
 			"param2": ''
 		}
 
 		token = client.service.GetTxnToken(**data)
-		return token
-
-	#결제모듈
-	def api_pay(self):
-		"""Pay method"""
-		digest = self.digest_parameters(
-			self.transaction_id,
-			self.format_amount(self.amount),
-			"PHP",
-			self.description,
-			self.email
-		)
-		
-		data = {
-			"merchantid": self.merchant_id,
-			"txnid": self.transaction_id,
-			"amount": self.format_amount(self.amount),
-			"ccy": "PHP",
-			"description": self.description,
-			"email": self.email,
-			"digest": digest,
-		}
-
-
-		return self.url + "/Pay.aspx?" + urllib.parse.urlencode(data)
-
-
-	def token_pay(self):
-		token = self.get_token()
 
 		return self.url + '/Pay.aspx?tokenid=' + token + "&procid=CUP"
 
