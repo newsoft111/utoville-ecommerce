6927db75f1d76b3ca553c6a99ffaa2c7387d5716
Ohreum <dhfma3394@naver.com>
2023-07-04 15:55:43 +0900

    backup

--- subscribe/views/user_views.py
+++ subscribe/views/user_views.py
@@ -6,8 +6,11 @@ from django.core import serializers
 # Create your views here.
 
 def user_subscription_about(request):
-	return render(request, 'user/subscribe/about.html')
+	return render(request, 'user/subscribe/subscription_about.html')
 
+def user_subscription_book_now(request):
+	return render(request, 'user/subscribe/subscription_book_now.html')
+	
 def user_subscription_check_list_view(request):
 	seo = {
 		'title': "Product List - Utoville",
