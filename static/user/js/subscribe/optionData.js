ff556019eeaf802922116b36775155ca9d228154
Ohreum <dhfma3394@naver.com>
2023-07-11 12:45:49 +0900

    option데이터, 금액계산 함수 따로 분류, pk값 구하는 로직 추가

---
+++ static/user/js/subscribe/optionData.js
@@ -0,0 +1,11 @@
+let inputOptionPrices = [
+	{ id : 0, aircon : "2000" },
+	{ id : 1, bathroom : "100" },
+	{ id: 2, bed : "50" },
+];
+let nonInputOptionPrices = [
+	{ id : 0, hood : "100", },
+	{ id : 1, greaseTrap : "50" }
+];
+
+export { inputOptionPrices, nonInputOptionPrices};
